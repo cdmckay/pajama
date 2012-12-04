06e67fccf51acf6f7da622a278e754d7509d3d51
cdmckay <me@cdmckay.org>
2012-12-03 23:19:30 -0500

    Updated form.js to use the submitHandler argument of the jQuery Validation plugin.

--- examples/jquery-validation-integration/form.js
+++ examples/jquery-validation-integration/form.js
@@ -11,33 +11,34 @@ jQuery(document).ready(function($) {
 
     $.get("rules.json", {}, "json")
         .done(function(rules) {
-            var validator = form.validate({ rules: rules });
-
             // Remove previous submit handler that blocked submission.
             form.off("submit");
 
-            form.on("submit", function() {
-                var skip = form.find("#skip_client_validation").is(":checked");
-                if (skip || validator.form()) {
-                    // Disable the submit button while posting.
-                    var submit = form.find(":submit").attr("disabled", "disabled");
-                    $.post(form.attr("action") || "", form.serialize(), "json")
-                        .done(function(response) {
-                            if (response.successful) {
-                                alert("Form submission was successful.")
-                            } else {
-                                alert("Form submission has failed.")
-                            }
-                        })
-                        .fail(function() {
-                            alert("Failed to communicate with server.");
-                        })
-                        .always(function() {
-                            // Enable the submit button after posting.
-                            submit.removeAttr("disabled");
-                        });
+            var validator = form.validate({
+                rules: rules,
+                submitHandler: function() {
+                    var skip = form.find("#skip_client_validation").is(":checked");
+                    if (skip || validator.form()) {
+                        // Disable the submit button while posting.
+                        var submit = form.find(":submit").attr("disabled", "disabled");
+                        $.post(form.attr("action") || "", form.serialize(), "json")
+                            .done(function(response) {
+                                if (response.successful) {
+                                    alert("Form submission was successful.")
+                                } else {
+                                    alert("Form submission has failed.")
+                                }
+                            })
+                            .fail(function() {
+                                alert("Failed to communicate with server.");
+                            })
+                            .always(function() {
+                                // Enable the submit button after posting.
+                                submit.removeAttr("disabled");
+                            });
+                    }
+                    return false;
                 }
-                return false;
             });
         });
 
