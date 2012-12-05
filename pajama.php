61cbe1d1b584459bdd55d281567884ab416db0cd
cdmckay <me@cdmckay.org>
2012-12-05 12:40:40 -0500

    Started adding PHPdoc documentation

--- pajama.php
+++ pajama.php
@@ -2,110 +2,51 @@
 
 namespace Pajama;
 
+/**
+ * A class for validating array models, typically the <code>$_POST</code> or <code>$_GET</code> superglobals.
+ */
 final class Validator {
 
+    /**
+     * @var array The model being validated.
+     */
     private $model;
+
+    /**
+     * @var array The normalized rules for this Validator instance.
+     */
     private $rules;
+
+    /**
+     * @var ValidatorContext The context for this Validator instance.
+     */
     private $context;
+
+    /**
+     * @var array An array containing all the validation methods.
+     */
     private static $methods = array();
 
+    /**
+     * Creates a new Validator instance.
+     *
+     * @param array $model The model to validate.
+     * @param array $rules The rules the model is validated against.
+     */
     private function __construct($model, $rules) {
         $this->model = self::flatten($model);
         $this->rules = $this->normalizeRules($rules);
         $this->context = new ValidatorContext($this);
     }
 
-    private function normalizeRules($rules) {
-        foreach ($rules as $field => $rule) {
-            $normalized_rule = $this->normalizeRule($rule);
-            foreach ($normalized_rule as $method_name => $param) {
-                if ($param === false) {
-                    unset($normalized_rule[$method_name]);
-                }
-            }
-            $rules[$field] = $normalized_rule;
-        }
-        return $rules;
-    }
-
-    private function normalizeRule($value) {
-        $normalized_value = $value;
-        if (is_string($value)) {
-            $normalized_value = array();
-            $method_names = preg_split('/\s/', $value);
-            foreach ($method_names as $method_name) {
-                $normalized_value[$method_name] = true;
-            }
-        }
-        return $normalized_value;
-    }
-
-    public static function validate($options) {
-        $noop = function() {};
-        $valid_handler = $options['validHandler'] ?: $noop;
-        $invalid_handler = $options['invalidHandler'] ?: $noop;
-
-        $validator = new Validator($options['model'], $options['rules']);
-        if ($validator->model()) {
-            $valid_handler($validator);
-        } else {
-            $invalid_handler($validator);
-        }
-
-        return $validator;
-    }
-
-    public static function getMethods() {
-        return self::$methods;
-    }
-
-    public static function addMethod($method_name, $method) {
-        self::$methods[$method_name] = $method;
-    }
-
-    public function field($name) {
-        $value = $this->model[$name];
-        $rule = $this->rules[$name];
-        $valid = true;
-        foreach ($rule as $method_name => $param) {
-            $method = self::$methods[$method_name];
-            $valid = $valid && (is_null($method) || $method($this->context, $value, $param));
-            if (!$valid) break;
-        }
-        return $valid;
-    }
-
-    public function model() {
-        $valid = true;
-        foreach ($this->rules as $name => $rule) {
-            $valid = $valid && $this->field($name);
-            if (!$valid) break;
-        }
-        return $valid;
-    }
-
-    public function invalids() {
-        $invalids = array();
-        foreach ($this->rules as $name => $rule) {
-            if (!$this->field($this->model, $name)) {
-                $invalids[] = $name;
-            }
-        }
-        return $invalids;
-    }
-
-    public function numberOfInvalids() {
-        return count($this->invalids($this->model));
-    }
-
-    public function getModel() {
-        return $this->model;
-    }
-
-    public function getRules() {
-        return $this->rules;
-    }
-
+    /**
+     * Flatten an array, appending all sub-array keys into the top-level name.
+     *
+     * For example, `$a['foo']['bar'] = 'baz'` becomes `$a['foo[bar]'] = 'baz'`.
+     *
+     * @param array $model
+     * @return array
+     */
     private static function flatten($model) {
         $repeat = false;
         foreach ($model as $name => $value) {
@@ -123,8 +64,152 @@ final class Validator {
         return $model;
     }
 
+    /**
+     * Validates the model, returning a reusable Validator object in the process.
+     *
+     * @param array $options
+     * @return Validator
+     */
+    public static function validate($options) {
+        $noop = function() {};
+        $valid_handler = $options['validHandler'] ?: $noop;
+        $invalid_handler = $options['invalidHandler'] ?: $noop;
+
+        $validator = new Validator($options['model'], $options['rules']);
+        if ($validator->model()) {
+            $valid_handler($validator);
+        } else {
+            $invalid_handler($validator);
+        }
+
+        return $validator;
+    }
+
+    /**
+     * Normalizes a rule array, removing all rules that are false.
+     *
+     * @param array $rules
+     * @return array
+     */
+    private function normalizeRules($rules) {
+        foreach ($rules as $field => $rule) {
+            $normalized_rule = $this->normalizeRule($rule);
+            foreach ($normalized_rule as $method_name => $param) {
+                if ($param === false) {
+                    unset($normalized_rule[$method_name]);
+                }
+            }
+            $rules[$field] = $normalized_rule;
+        }
+        return $rules;
+    }
+
+    /**
+     * Normalizes a rule, converting string rules into their array equivalents.
+     *
+     * @param array|string $value
+     * @return array
+     */
+    private function normalizeRule($value) {
+        $normalized_value = $value;
+        if (is_string($value)) {
+            $normalized_value = array();
+            $method_names = preg_split('/\s/', $value);
+            foreach ($method_names as $method_name) {
+                $normalized_value[$method_name] = true;
+            }
+        }
+        return $normalized_value;
+    }
+
+    /**
+     * Validates a field, returning true if valid, false otherwise.
+     *
+     * This is the rough equivalent of the "element" method in the jQuery Validation plugin.
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function field($name) {
+        $value = $this->model[$name];
+        $rule = $this->rules[$name];
+        $valid = true;
+        foreach ($rule as $method_name => $param) {
+            $method = self::$methods[$method_name];
+            $valid = $valid && (is_null($method) || $method($this->context, $value, $param));
+            if (!$valid) break;
+        }
+        return $valid;
+    }
+
+    /**
+     * @return bool
+     */
+    public function model() {
+        $valid = true;
+        foreach ($this->rules as $name => $rule) {
+            $valid = $valid && $this->field($name);
+            if (!$valid) break;
+        }
+        return $valid;
+    }
+
+    /**
+     * @return array
+     */
+    public function invalids() {
+        $invalids = array();
+        foreach ($this->rules as $name => $rule) {
+            if (!$this->field($this->model, $name)) {
+                $invalids[] = $name;
+            }
+        }
+        return $invalids;
+    }
+
+    /**
+     * @return int
+     */
+    public function numberOfInvalids() {
+        return count($this->invalids($this->model));
+    }
+
+    /**
+     * @return array
+     */
+    public function getModel() {
+        return $this->model;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRules() {
+        return $this->rules;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getMethods() {
+        return self::$methods;
+    }
+
+    /**
+     * Adds a new validation method.
+     *
+     * @param string $method_name The name of the method, i.e. 'range' or 'creditcard'.
+     * @param callable $method The validation method, typically an anonymous function.
+     */
+    public static function addMethod($method_name, $method) {
+        self::$methods[$method_name] = $method;
+    }
+
 }
 
+/**
+ * Provided to each validation method to provide useful methods and validation context information.
+ */
 final class ValidatorContext {
 
     private $validator;
